07fae51aaf2a7cd40645427109e7fbc935dc4384
Gautam Jain <19173627+gjain574@users.noreply.github.com>
2025-01-29 00:45:12 +0530

    Update broken image for skills

--- src/content/skills.ts
+++ src/content/skills.ts
@@ -38,7 +38,7 @@ export const skills: Skill[] = [
   },
   {
     name: "Tailwind CSS",
-    image: "https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg",
+    image: "https://tailwindcss.com/favicons/apple-touch-icon.png?v=4",
     category: 'frontend',
     icon: '🎨',
     description: 'Utility-first CSS framework'
