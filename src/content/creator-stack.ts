19c1b5685e56399f2427f011362a77c0d2c62326
Gautam Jain <19173627+gjain574@users.noreply.github.com>
2025-01-29 00:51:48 +0530

    Add content for creator stack

---
+++ src/content/creator-stack.ts
@@ -0,0 +1,46 @@
+import { CreatorStack } from "@/types/creator-stack";
+
+export const creatorStackContent: CreatorStack[] = [
+    {
+        name: 'Tella',
+        link: 'https://www.tella.com/',
+        description: 'The screen recorder that edits videos for you',
+        imageUrl: '/images/creator-stack/tella.png',
+    },
+    {
+        name: 'Shots',
+        link: 'https://shots.so/',
+        description: 'Create Amazing Mockups',
+        imageUrl: '/images/creator-stack/shots.png',
+    },
+    {
+        name: 'Lovable',
+        link: 'https://www.lovable.dev/',
+        description: 'Create Full Stack Apps',
+        imageUrl: '/images/creator-stack/lovable.png',
+    },
+    {
+        name: 'Figma',
+        link: 'https://www.figma.com/',
+        description: 'Design Tool',
+        imageUrl: '/images/creator-stack/figma.png',
+    },
+    {
+        name: 'Prompt2MVP',
+        link: 'https://chromewebstore.google.com/detail/prompt2mvp/dfojilkhdfjdlmhpchfajagmiamindgh',
+        description: 'Combine screenshots of design inspiration with your prompt',
+        imageUrl: '/images/creator-stack/prompt2mvp.png',
+    },
+    {
+        name: 'Compress PNG',
+        link: 'https://compresspng.com/',
+        description: 'Compress PNG images',
+        imageUrl: '/images/creator-stack/compresspng.png',
+    },
+    {
+        name: 'Extension Icon Generator',
+        link: 'https://alexleybourne.github.io/chrome-extension-icon-generator/',
+        description: 'Generate icons for your Chrome extensions',
+        imageUrl: '/images/creator-stack/extension-icon-generator.png',
+    }
+]
